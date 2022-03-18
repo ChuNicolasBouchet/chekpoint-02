12f33c62247fd1089615bbe8388f5f2a46897c98
unknown <chunicolasbouchet@gmail.com>
2022-03-18 14:03:01 +0100

    je pense pas être loins mais ça marche pas

--- react-games/src/components/GameList.js
+++ react-games/src/components/GameList.js
@@ -3,21 +3,35 @@ import axios from 'axios';
 import Game from "./Game";
 
 function GameList() {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState([]);
+    const [showRatedOnly, setShowRatedOnly] = useState(false);
+        
+
     useEffect(() => {
         axios.get('https://apis.wilders.dev/wild-games/games/')
-            .then(response => setGames(response.data))
+            .then(response => setGames(response.data)) 
             .catch(error => console.log(error))
-
     }, []);
+
+    const handleRatedGamesClick = () => {
+        setShowRatedOnly((show) => !show);
+    }
     
     return (
-        <div>
-            {games.map((game, id) => {
-                return <Game key={game.id} name={game.name} released={game.released} background_image={game.background_image} rating={game.rating}/>
-            })}
-
-        </div>
+        <>
+            <div className="wrapper_btn_filter_rating">
+                <button type="button" onclick={handleRatedGamesClick}>
+                    {showRatedOnly ? 'All' : 'Above 4.5'} rated games
+                </button>
+            </div>
+            <div className="wrapper_list_games">    
+                {games
+                    .filter((game) => !showRatedOnly || game.rating > 4.5)
+                    .map((game) => {
+                    return <Game key={game.id} name={game.name} released={game.released} background_image={game.background_image} rating={game.rating}/>
+                })}
+            </div>
+        </>
     )
 }
 
